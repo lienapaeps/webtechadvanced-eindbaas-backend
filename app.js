c6144f392726dbc3c3f26fe131d19b12e23f730c
lienapaeps <paepsliena@gmail.com>
2022-04-28 11:48:29 +0200

    feat: require mongoose

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const apiTransfersRouter = require("./routes/api/v1/transfers");
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/coinapp');
 
 var app = express();
 
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/api/v1/transfers", apiTransfersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
