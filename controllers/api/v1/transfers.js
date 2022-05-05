258aecd77fa803bd541f61f48fd3a7d66df9897d
lienapaeps <paepsliena@gmail.com>
2022-05-05 12:14:32 +0200

    feat: create transfer

--- controllers/api/v1/transfers.js
+++ controllers/api/v1/transfers.js
@@ -2,8 +2,30 @@ const Transfer = require('../../../models/transfer');
 
 // POST transfer
 const create = (req, res) => {
-let transfer = new Transfer();
-console.log(req.body);
+    let transfer = new Transfer();
+    transfer.sender = req.body.sender;
+    transfer.receiver = req.body.receiver;
+    transfer.amount = req.body.amount;
+    transfer.message = req.body.message;
+
+    transfer.save((err, doc) => {
+        if (!err) {
+            res.json( {
+                "status": "Success",
+                "data": {
+                    "transfer": doc
+                }
+            });
+        } else if (err) {
+            res.json( {
+                "status": "Error",
+                "message": "Error while creating transfer"
+            });
+        }
+    } );
+
+    // console.log(req.body);
+    // res.send("POSTING new message");
 }
 
 // GET transfers
