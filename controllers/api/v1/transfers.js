8b85295ff9b683712ef853ea39e377d01ac80c60
lienapaeps <paepsliena@gmail.com>
2022-05-05 11:08:23 +0200

    fix: bug

--- controllers/api/v1/transfers.js
+++ controllers/api/v1/transfers.js
@@ -1,7 +1,27 @@
+const Transfer = require('../../models/Transfer');
+
 // POST transfer
+const create = (req, res) => {
+let transfer = new Transfer();
+console.log(req.body);
+}
 
 // GET transfers
+const getTransfers = (req, res) => {
+
+}
 
 // GET transfer with id
+const getTransferById = (req, res) => {
+
+}
 
 // GET leaderboard
+const getLeaderboard = (req, res) => {
+
+}
+
+// module.exports.create = create;
+// module.exports.getTransfers = getTransfers;
+// module.exports.getTransferById = getTransferById;
+// module.exports.getLeaderboard = getLeaderboard;
--- models/Transfer.js
+++ models/Transfer.js
@@ -2,19 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const transferSchema = new Schema({
-    // sender, receiver, amount, status
+    // sender, receiver, amount, message, status
     sender: {
 
     },
     receiver: {
-
+        
     },
     amount: { 
-
+        type: Number,
+        required: true
     },
-    status: {
-
-    }
+    message: {
+        type: String,
+        required: true
+    },
+    status: Boolean
 });
 
 const Transfer = mongoose.model('Transfer', transferSchema);
