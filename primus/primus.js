75bbe1789001cad7a9501a7a947d1d13066be107
lienapaeps <paepsliena@gmail.com>
2022-05-29 18:26:07 +0200

    feat: add procfile

--- primus/primus.js
+++ primus/primus.js
@@ -2,10 +2,11 @@ const Primus = require("primus");
 const { PrimusError } = require("primus/errors");
 
 let go = (server) => {
-    let primus = new Primus(server, /*options*/);
+    let primus = new Primus(server, {/*options*/});
 
+    // elke geconnecteerde gebruiker is een spark
     primus.on('connection', (spark) => {
-        console.log("🔥🔥🔥");
+        console.log("Received spark 🔥");
         spark.on('data', (data) => {
             console.log(data);
             primus.write(data);
